b2fddba7f89725992e55edc140ea4ba84b8e5ce0
Joe O'Neil <apjoey914@gmail.com>
2022-02-01 19:30:55 -0500

    [Julia] updated purge ball command
    completed todo

--- src/main/java/frc/robot/commands/PurgeBall.java
+++ src/main/java/frc/robot/commands/PurgeBall.java
@@ -6,13 +6,23 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
+import frc.robot.Constants;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
 
 public class PurgeBall extends CommandBase {
+  public Intake intake;
+  public Feeder feeder;
+
   // TODO: finish this
   /** Creates a new BallPurge. */
   public PurgeBall(int side) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(RobotContainer.intake[side], RobotContainer.feeder[side], RobotContainer.shooter[side]);
+    // addRequirements(RobotContainer.intake[side], RobotContainer.feeder[side],
+    // RobotContainer.shooter[side]);
+    intake = RobotContainer.intake[side];
+    feeder = RobotContainer.feeder[side];
+    addRequirements(intake, feeder);
   }
 
   // Called when the command is initially scheduled.
@@ -23,11 +33,15 @@ public class PurgeBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    intake.purge();
+    feeder.setPercentOutput(Constants.FEEDER_PURGE_SPEED);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.stop();
+    feeder.setPercentOutput(0);
   }
 
   // Returns true when the command should end.
