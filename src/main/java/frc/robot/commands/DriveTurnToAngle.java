e9ded71a1ca136953b58c2b05b036b4fda101d70
rob-heslin <Rob.Heslin@gmail.com>
2022-02-12 08:23:32 -0500

    [Rob] added DriveTurnToAngle, a function to test the rotationPidController

---
+++ src/main/java/frc/robot/commands/DriveTurnToAngle.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class DriveTurnToAngle extends CommandBase {
+  double targetAngle;
+  ProfiledPIDController rotationController;
+  /** Creates a new DriveTurnToAngle. */
+  public DriveTurnToAngle(double targetAngle) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.drive);
+    rotationController = new ProfiledPIDController(Constants.DRIVE_ROTATION_CONTROLLER_P,
+        Constants.DRIVE_ROTATION_CONTROLLER_I, Constants.DRIVE_ROTATION_CONTROLLER_D,
+        new TrapezoidProfile.Constraints(Constants.DRIVE_MAX_ANGULAR_VELOCITY, Constants.DRIVE_MAX_ANGULAR_ACCEL));
+    rotationController.enableContinuousInput(-180, 180);
+    this.targetAngle = targetAngle;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double rotationOutput = rotationController.calculate(RobotContainer.drive.getAngleDegrees(), targetAngle);
+    RobotContainer.drive.drive(0, 0, rotationOutput, false);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.drive.drive(0, 0, 0, false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Math.abs(targetAngle - RobotContainer.drive.getAngleDegrees()) < 2;
+  }
+}
